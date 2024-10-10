98ac04961d2357dcc38e0ed04b5ace14bebdd2dc
justlikesh <lpsac@naver.com>
2024-10-10 19:12:34 +0900

    Feat : 프로그래밍으로 제어하는 Filtering - 전체 사용자 조회
    - 전체 유저 데이터 값을 adminUser 데이터 값으로 바꿔서 프로포티 복사를 해서 전체값 반환

--- src/main/java/kr/co/joneconsulting/myrestfulservice/controller/AdminUserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/controller/AdminUserController.java
@@ -11,12 +11,14 @@ import kr.co.joneconsulting.myrestfulservice.dao.UserDaoService;
 import kr.co.joneconsulting.myrestfulservice.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -46,6 +48,29 @@ public class AdminUserController {
         FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);  // 어디다가 적용할건지
 
         MappingJacksonValue mapping = new MappingJacksonValue(adminUser);  // FilterProvider 로 결과 데이터를 만들어서 반환
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsers(){
+        List<User> users = service.findAll();
+
+        List<AdminUser> adminUsers = new ArrayList<>();
+        AdminUser adminUser = null;
+        for(User user : users){             // 전체 유저 데이터 값을
+            adminUser = new AdminUser();        //adminuser 데이터 값으로 바꿔서
+            BeanUtils.copyProperties(user, adminUser); //프로포티를 복사해서 리스트에 추가하는 작업
+            adminUsers.add(adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
         mapping.setFilters(filters);
 
         return mapping;
